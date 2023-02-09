091222e5eaf9b18bf1ebd53a7a7cbf4e4ea477b3
maksimvrublevskij <maksimvrublevskij2022@gmail.com>
2023-02-10 00:05:04 +0300

    second save

--- class.py
+++ class.py
@@ -10,10 +10,21 @@ class Mage:
         self.mana = 100
 
 
+class Archer:
+    def __init__(self):
+        self.health = 50
+        self.arrows = 50
+
+
 unit1 = Warrior()
 unit2 = Warrior()
 unit3 = Mage()
+unit4 = Archer()
+unit5 = Archer()
+
 
 print(unit1.__dict__)
 print(unit2.__dict__)
 print(unit3.__dict__)
+print(unit4.__dict__)
+print(unit5.__dict__)
